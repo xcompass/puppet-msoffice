a3e094992a227d15d615b55b6503e49ff6ad74a5
Pan Luo <pan.luo@ubc.ca>
2014-05-03 02:46:00 -0700

    Update tests

--- spec/classes/msoffice_spec.rb
+++ spec/classes/msoffice_spec.rb
@@ -12,14 +12,14 @@ describe 'msoffice' do
 
   it do
     should contain_class('msoffice')
-    should_not contain_package('msoffice')
+    should_not contain_package("MSOffice-#{version}")
   end
 
   context "credential is provided" do
     let(:params) {{:username => "test", :password => "test"}}
 
     it do
-      should contain_package('msoffice').with({
+      should contain_package("MSOffice-#{version}").with({
         :provider => 'pkgdmg',
         :source   => "http://test:test@artifactory.ctlt.ubc.ca/artifactory/ctlt-protected/Office/Office_Mac_Standard_#{version}.dmg",
       })
